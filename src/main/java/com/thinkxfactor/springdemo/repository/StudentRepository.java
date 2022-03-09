21a82c3b31537f5ae1d84c943e188ce38adee065
Soumyadeep <mukherjeesoumyadeep28@gmail.com>
2022-03-10 00:23:46 +0530

    Student repository updated

--- src/main/java/com/thinkxfactor/springdemo/repository/StudentRepository.java
+++ src/main/java/com/thinkxfactor/springdemo/repository/StudentRepository.java
@@ -1,9 +1,14 @@
 package com.thinkxfactor.springdemo.repository;
 
+import java.util.Optional;
+
 import com.thinkxfactor.springdemo.entity.Student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+    Student findByUserName(String username);
+
+    Optional<Student> findByUsernameAndPassword(String username, String password);
 }
